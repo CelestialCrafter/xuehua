83ae61ed0a68cd2f2fadfbaa088d3495a7c9b1b1
CelestialCrafter <celestialcraftermc@gmail.com>
2025-08-22 01:47:08 -0500

    (executor/http) impl serialize (executor/http) impl some traits for request (executor/bubblewrap) implement serialize (executor/bubblewrap) impl some traits for request

--- crates/executor-http/src/lib.rs
+++ crates/executor-http/src/lib.rs
@@ -1,16 +1,10 @@
 use std::{
-    fmt,
-    marker::PhantomData,
     path::{Component, PathBuf},
-    str::FromStr,
     sync::Arc,
 };
 
 use log::debug;
-use serde::{
-    Deserialize, Deserializer,
-    de::{Error as DeError, Visitor},
-};
+use serde::{Deserialize, Serialize};
 use ureq::{
     Agent,
     config::Config,
@@ -21,39 +15,52 @@ use xh_reports::prelude::*;
 
 const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
-// stolen from https://users.rust-lang.org/t/serde-fromstr-on-a-field/99457/5
-fn deserialize_from_str<'de, T, D>(deserializer: D) -> StdResult<T, D::Error>
-where
-    T: FromStr,
-    T::Err: fmt::Display,
-    D: Deserializer<'de>,
-{
-    struct Helper<S>(PhantomData<S>);
-    impl<'de, S> Visitor<'de> for Helper<S>
+mod serde_display {
+    use std::{fmt, marker::PhantomData, str::FromStr};
+
+    use serde::{
+        Deserializer, Serializer,
+        de::{Error as DeError, Visitor},
+    };
+
+    // stolen from https://users.rust-lang.org/t/serde-fromstr-on-a-field/99457/5
+    pub fn deserialize<'de, T, D>(deserializer: D) -> Result<T, D::Error>
     where
-        S: FromStr,
-        S::Err: fmt::Display,
+        T: FromStr,
+        T::Err: fmt::Display,
+        D: Deserializer<'de>,
     {
-        type Value = S;
+        struct Helper<S>(PhantomData<S>);
+        impl<'de, S> Visitor<'de> for Helper<S>
+        where
+            S: FromStr,
+            S::Err: fmt::Display,
+        {
+            type Value = S;
 
-        fn expecting(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-            write!(fmt, "a string")
+            fn expecting(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+                write!(fmt, "a string")
+            }
+
+            fn visit_str<E: DeError>(self, value: &str) -> Result<Self::Value, E> {
+                value.parse::<Self::Value>().map_err(E::custom)
+            }
         }
 
-        fn visit_str<E: DeError>(self, value: &str) -> StdResult<Self::Value, E> {
-            value.parse::<Self::Value>().map_err(E::custom)
-        }
+        deserializer.deserialize_str(Helper(PhantomData))
     }
 
-    deserializer.deserialize_str(Helper(PhantomData))
+    pub fn serialize<T: fmt::Display, S: Serializer>(v: &T, s: S) -> Result<S::Ok, S::Error> {
+        s.serialize_str(&v.to_string())
+    }
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct HttpRequest {
     pub path: PathBuf,
-    #[serde(deserialize_with = "deserialize_from_str")]
+    #[serde(with = "serde_display")]
     pub url: Uri,
-    #[serde(deserialize_with = "deserialize_from_str")]
+    #[serde(with = "serde_display")]
     pub method: Method,
 }
 
