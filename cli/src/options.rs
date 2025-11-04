7b469886f4893c0308c28c6d804b04b1be62bd9f
CelestialCrafter <celestialcraftermc@gmail.com>
2025-11-04 10:36:37 -0600

    (cli/opts) impl directory location config

--- cli/src/options.rs
+++ cli/src/options.rs
@@ -1,15 +1,27 @@
 pub mod base;
 pub mod cli;
 
-use std::sync::LazyLock;
+use eyre::Result;
 
-pub static OPTIONS: LazyLock<Options> = LazyLock::new(|| Options {
-    // TODO: use .run_inner() and completely overhaul the display
-    cli: cli::Options::options().run(),
-    base: base::BaseOptions::default(),
-});
+use std::sync::OnceLock;
+
+pub static OPTIONS: OnceLock<Options> = OnceLock::new();
+
+pub fn get_opts() -> &'static Options {
+    OPTIONS.get().expect("options should be initialized")
+}
 
 pub struct Options {
     pub cli: cli::Options,
     pub base: base::BaseOptions,
+}
+
+impl Options {
+    pub fn run() -> Result<Self> {
+        Ok(Options {
+            // TODO: use .run_inner() and completely overhaul the display
+            cli: cli::Options::options().run(),
+            base: base::BaseOptions::run()?,
+        })
+    }
 }
