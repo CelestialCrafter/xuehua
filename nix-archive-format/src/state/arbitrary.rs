1f95ed3b314e7f8be3a26813a584832b77103777
CelestialCrafter <celestialcraftermc@gmail.com>
2025-11-08 17:56:29 -0600

    (nix-archive-format) implement arbitrary for events

---
+++ nix-archive-format/src/state/arbitrary.rs
@@ -0,0 +1,65 @@
+use std::ops::ControlFlow;
+
+use arbitrary::Arbitrary;
+
+use crate::state::Event;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct ArbitraryObject(pub Vec<Event>);
+
+impl Arbitrary<'_> for ArbitraryObject {
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        let mut events = Vec::new();
+        match u.choose_index(3)? {
+            0 => {
+                let mut size = u.arbitrary_len::<&[u8]>()?;
+                events.push(Event::Regular {
+                    executable: u.arbitrary()?,
+                    size: size as u64,
+                });
+
+                while size != 0 {
+                    let chunk_size = u.int_in_range(1..=size)?;
+                    size -= chunk_size;
+                    let chunk = u.bytes(chunk_size)?.to_vec();
+                    events.push(Event::RegularContentChunk(chunk));
+                }
+            }
+            1 => events.push(Event::Symlink {
+                target: u.arbitrary()?,
+            }),
+            2 => {
+                events.push(Event::Directory);
+
+                const MAX_FILES: u32 = 8;
+                u.arbitrary_loop(None, Some(MAX_FILES), |u| {
+                    events.push(Event::DirectoryEntry {
+                        name: u.arbitrary()?,
+                    });
+                    events.extend(Self::arbitrary(u)?.0);
+
+                    Ok(ControlFlow::Continue(()))
+                })?;
+
+                events.push(Event::DirectoryEnd);
+            }
+            _ => unreachable!(),
+        }
+
+        Ok(Self(events))
+    }
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct ArbitraryNar(pub Vec<Event>);
+
+impl Arbitrary<'_> for ArbitraryNar {
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> Result<Self, arbitrary::Error> {
+        let mut events = Vec::with_capacity(1);
+        events.push(Event::Header);
+        events.extend(ArbitraryObject::arbitrary(u)?.0);
+        Ok(Self(events))
+    }
+}
