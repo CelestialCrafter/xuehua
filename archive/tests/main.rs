a36234e43738a1b9ab54a34e2dc14d28a438951c
CelestialCrafter <celestialcraftermc@gmail.com>
2025-12-26 21:11:42 -0600

    (archive/tests) add reader/writer tests

--- archive/tests/main.rs
+++ archive/tests/main.rs
@@ -2,81 +2,101 @@ use std::ffi::OsStr;
 
 use arbitrary::Arbitrary;
 use arbtest::arbtest;
+use bytes::Bytes;
 use include_dir::include_dir;
 use libtest_mimic::{Arguments, Trial};
 use xh_archive::Event;
 
-use crate::utils::{
-    ArbitraryArchive, BenchmarkOptions, benchmark, decode, encode, make_temp, setup,
-};
+use crate::utils::{ArbitraryArchive, BenchmarkOptions, benchmark, decode, encode, setup};
 
 mod utils;
 
 #[cfg(feature = "std")]
 #[inline]
-fn pack_unpack_roundtrip(events: Vec<Event>) {
-    let (path, _temp) = make_temp();
+fn pack_unpack_roundtrip(events: &Vec<Event>) {
+    let (path, _temp) = utils::make_temp();
 
-    utils::unpack(&path, &events);
-    assert_eq!(events, utils::pack(&path));
+    utils::unpack(&path, events);
+    assert_eq!(events, &utils::pack(&path));
 }
 
 #[cfg(all(feature = "std", feature = "mmap"))]
 #[inline]
-fn mmap_pack_unpack_roundtrip(events: Vec<Event>) {
-    let (path, _temp) = make_temp();
+fn mmap_pack_unpack_roundtrip(events: &Vec<Event>) {
+    let (path, _temp) = utils::make_temp();
 
-    utils::unpack_mmap(&path, &events);
-    assert_eq!(events, utils::pack_mmap(&path));
+    utils::unpack_mmap(&path, events);
+    assert_eq!(events, &utils::pack_mmap(&path));
 }
 
 #[inline]
-fn enc_dec_roundtrip(events: Vec<Event>) {
-    assert_eq!(events, decode(&encode(&events)));
+fn enc_dec_roundtrip(events: &Vec<Event>) {
+    assert_eq!(events, &decode(&mut encode(events)));
+}
+
+#[cfg(feature = "std")]
+#[inline]
+fn io_enc_dec_roundtrip(events: &Vec<Event>) {
+    use bytes::{Buf, BufMut, BytesMut};
+
+    let mut writer = BytesMut::new().writer();
+    utils::encode_writer(events, &mut writer);
+
+    let mut reader = writer.into_inner().reader();
+    assert_eq!(events, &utils::decode_reader(&mut reader));
 }
 
 #[inline]
 fn arbitrary_trials() -> impl Iterator<Item = Trial> {
     fn trial<F>(name: &str, runner: F) -> Trial
     where
-        F: Fn(&mut arbitrary::Unstructured<'_>) -> Result<(), arbitrary::Error>,
-        F: Send,
-        F: 'static,
+        F: Fn(&Vec<Event>),
+        F: Send + Sync + 'static,
     {
-        Trial::test(name, || {
-            arbtest(runner).run();
+        Trial::test(name, move || {
+            arbtest(|u| {
+                runner(&ArbitraryArchive::arbitrary(u)?.events);
+                Ok(())
+            })
+            .run();
             Ok(())
         })
     }
 
-    [trial("enc-dec-arbitrary", |u| {
-        enc_dec_roundtrip(ArbitraryArchive::arbitrary(u)?.events);
-        Ok(())
-    })]
+    [
+        trial("enc-dec-arbitrary", enc_dec_roundtrip),
+        #[cfg(feature = "std")]
+        trial("io-enc-dec-arbitrary", io_enc_dec_roundtrip),
+    ]
     .into_iter()
     .map(|trial| trial.with_kind("arbitrary"))
 }
 
 #[inline]
 fn blob_trials() -> impl Iterator<Item = Trial> {
-    let trials = |name, contents| {
-        let events = Box::leak(Box::new(decode(contents)));
+    let trials = |name, mut contents| {
+        let events = Box::leak(Box::new(decode(&mut contents)));
         let options = BenchmarkOptions::default();
 
         [
             Trial::bench(
                 format!("enc-dec-{name}"),
-                benchmark(|| Ok(enc_dec_roundtrip(events.to_vec())), options),
+                benchmark(|| enc_dec_roundtrip(events), options),
+            ),
+            #[cfg(feature = "std")]
+            Trial::bench(
+                format!("io-enc-dec-{name}"),
+                benchmark(|| io_enc_dec_roundtrip(events), options),
             ),
             #[cfg(feature = "std")]
             Trial::bench(
                 format!("pack-unpack-{name}"),
-                benchmark(|| Ok(pack_unpack_roundtrip(events.to_vec())), options),
+                benchmark(|| pack_unpack_roundtrip(events), options),
             ),
             #[cfg(all(feature = "std", feature = "mmap"))]
             Trial::bench(
                 format!("mmap-pack-unpack-{name}"),
-                benchmark(|| Ok(mmap_pack_unpack_roundtrip(events.to_vec())), options),
+                benchmark(|| Ok(mmap_pack_unpack_roundtrip(events)), options),
             ),
         ]
     };
@@ -87,7 +107,7 @@ fn blob_trials() -> impl Iterator<Item = Trial> {
         .map(move |file| {
             trials(
                 file.path().file_stem().unwrap().to_string_lossy(),
-                file.contents(),
+                Bytes::copy_from_slice(file.contents()),
             )
         })
         .flatten()
@@ -95,8 +115,8 @@ fn blob_trials() -> impl Iterator<Item = Trial> {
 }
 
 fn main() {
+    let trials = blob_trials().chain(arbitrary_trials()).collect();
     setup();
 
-    let trials = blob_trials().chain(arbitrary_trials()).collect();
     libtest_mimic::run(&Arguments::from_args(), trials).exit()
 }
