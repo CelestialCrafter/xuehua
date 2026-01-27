a89cb3b9cffab0906c1cd57c7bc04e936a66f9ec
CelestialCrafter <celestialcraftermc@gmail.com>
2026-01-26 22:26:10 -0600

    (global) move crates into their own dir (global) fix some compile/clippy errors (global) update crates/flake (store/sqlite) rename store-local to store-sqlite

---
+++ crates/executor-bubblewrap/src/lib.rs
@@ -0,0 +1,166 @@
+use std::{
+    process::{ExitStatus, Output},
+    sync::Arc,
+};
+
+use log::debug;
+use serde::Deserialize;
+
+use xh_engine::{builder::InitializeContext, executor::Executor};
+use xh_reports::prelude::*;
+
+#[derive(Debug, IntoReport)]
+#[message("external command failed")]
+#[context(status)]
+#[attachment(stderr)]
+pub struct CommandError {
+    status: ExitStatus,
+    stderr: String,
+}
+
+#[derive(Default, Debug, IntoReport)]
+#[message("could not execute request")]
+pub struct Error;
+
+#[derive(Default, Debug, Deserialize)]
+#[serde(default)]
+pub struct CommandRequest {
+    program: String,
+    working_dir: Option<String>,
+    arguments: Vec<String>,
+    environment: Vec<(String, String)>,
+}
+
+#[derive(Debug)]
+pub struct Options {
+    network: bool,
+    add_capabilities: Vec<String>,
+    drop_capabilities: Vec<String>,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            network: true,
+            add_capabilities: Default::default(),
+            drop_capabilities: Default::default(),
+        }
+    }
+}
+
+/// A command executor using [`bubblewrap`](https://github.com/containers/bubblewrap) for sandboxing
+///
+/// # Security/Sandboxing
+///
+/// This executor attempts to enforce a security boundary to ensure reproducability
+/// and minimize the impact of malicious build scripts on the host system.
+///
+// NOTE: ensure this list stays in sync with the code
+/// By default, the following safety related `bubblewrap` flags are enabled by default:
+/// - `--new-session`
+/// - `--unshare-all`
+/// - `--clearenv`
+///
+/// # Command Runner
+///
+/// To execute multiple commands within the sandbox, this executor bundles a command runner.
+/// The runner is embedded within the library at compile-time, and is controlled via stdin/stdout.
+pub struct BubblewrapExecutor {
+    ctx: Arc<InitializeContext>,
+    options: Options,
+}
+
+impl BubblewrapExecutor {
+    #[inline]
+    pub fn new(ctx: Arc<InitializeContext>, options: Options) -> Self {
+        Self { ctx, options }
+    }
+}
+
+impl Executor for BubblewrapExecutor {
+    const NAME: &'static str = "bubblewrap@xuehua/executors";
+    type Request = CommandRequest;
+    type Error = Error;
+
+    async fn execute(&mut self, request: Self::Request) -> Result<(), Self::Error> {
+        debug!(
+            "running command {:?}",
+            std::iter::once(request.program.clone())
+                .chain(request.arguments.clone())
+                .collect::<Vec<_>>()
+                .join(" "),
+        );
+
+        let mut sandboxed = tokio::process::Command::new("bwrap");
+
+        // essentials
+        sandboxed
+            .arg("--bind")
+            .arg(&self.ctx.environment)
+            .arg("/")
+            .args([
+                // TODO: move busybox bootstrap to its own package
+                "--ro-bind",
+                "busybox-bootstrap",
+                "/busybox",
+                "--proc",
+                "/proc",
+                "--dev",
+                "/dev",
+            ]);
+
+        // restrictions
+        sandboxed.args([
+            "--new-session",
+            "--die-with-parent",
+            "--clearenv",
+            "--unshare-all",
+        ]);
+
+        sandboxed.args(
+            self.options
+                .add_capabilities
+                .iter()
+                .flat_map(|cap| ["--cap-add", cap]),
+        );
+
+        sandboxed.args(
+            self.options
+                .drop_capabilities
+                .iter()
+                .flat_map(|cap| ["--cap-drop", cap]),
+        );
+
+        if self.options.network {
+            sandboxed.arg("--share-net");
+        }
+
+        // command payload
+        if let Some(working_dir) = request.working_dir {
+            sandboxed.arg("--chdir").arg(working_dir);
+        }
+
+        for (key, value) in request.environment {
+            sandboxed.args(["--setenv", &key, &value]);
+        }
+
+        sandboxed
+            .arg("--")
+            .arg(request.program)
+            .args(request.arguments);
+
+        let Output {
+            status,
+            stderr,
+            stdout: _,
+        } = sandboxed.output().await.wrap()?;
+        status
+            .success()
+            .then_some(())
+            .ok_or(CommandError {
+                status,
+                stderr: String::from_utf8_lossy(&stderr).to_string(),
+            })
+            .wrap()
+    }
+}
---
+++ crates/store-sqlite/src/lib.rs
@@ -0,0 +1,320 @@
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    path::PathBuf,
+};
+
+use bytes::Bytes;
+use educe::Educe;
+use jiff::Timestamp;
+use log::debug;
+use rusqlite::{Connection, OptionalExtension, named_params};
+use tokio::sync::{mpsc, oneshot};
+use xh_archive::{Event, decoding::Decoder};
+use xh_engine::{
+    planner::PackageId,
+    store::{ArtifactId, Error, Store, StoreArtifact, StorePackage},
+    utils::{ensure_dir, random_hash},
+};
+use xh_reports::{compat::StdCompat, prelude::*};
+
+struct Queries;
+
+impl Queries {
+    const REGISTER_ARTIFACT: &'static str =
+        "INSERT INTO artifacts (id, created_at) VALUES (:id, :created_at)";
+    const REGISTER_PACKAGE: &'static str =
+        "INSERT INTO packages (id, artifact, created_at) VALUES (:id, :artifact, :created_at)";
+    const GET_PACKAGE: &'static str =
+        "SELECT 1 FROM packages WHERE id IS :id ORDER BY created_at DESC";
+    const GET_ARTIFACT: &'static str = "SELECT 1 FROM artifacts WHERE id IS :id";
+}
+
+#[derive(Educe)]
+#[educe(Debug)]
+enum Task {
+    RegisterPackage {
+        package: PackageId,
+        artifact: ArtifactId,
+        #[educe(Debug(ignore))]
+        channel: oneshot::Sender<Result<StorePackage, Error>>,
+    },
+    GetPackage {
+        package: PackageId,
+        #[educe(Debug(ignore))]
+        channel: oneshot::Sender<Result<Option<StorePackage>, Error>>,
+    },
+    RegisterArtifact {
+        #[educe(Debug(ignore))]
+        archive: Vec<Event>,
+        root: PathBuf,
+        #[educe(Debug(ignore))]
+        channel: oneshot::Sender<Result<StoreArtifact, Error>>,
+    },
+    GetArtifact {
+        artifact: ArtifactId,
+        #[educe(Debug(ignore))]
+        channel: oneshot::Sender<Result<Option<StoreArtifact>, Error>>,
+    },
+    DecodeArtifact {
+        artifact: ArtifactId,
+        root: PathBuf,
+        #[educe(Debug(ignore))]
+        channel: oneshot::Sender<Result<Option<Vec<Event>>, Error>>,
+    },
+    Shutdown,
+}
+
+fn register_package(
+    db: &mut Connection,
+    package: PackageId,
+    artifact: ArtifactId,
+) -> Result<StorePackage, Error> {
+    db.execute(
+        Queries::REGISTER_PACKAGE,
+        named_params! {
+            ":id": package.as_bytes(),
+            ":artifact": artifact.as_bytes(),
+            ":created_at": Timestamp::now(),
+        },
+    )
+    .wrap()?;
+
+    db.prepare_cached(Queries::GET_PACKAGE)
+        .wrap()?
+        .query_one(named_params! { ":id": package.as_bytes() }, |row| {
+            Ok(StorePackage {
+                id: PackageId::from_bytes(row.get("id")?),
+                artifact: ArtifactId::from_bytes(row.get("artifact")?),
+                created_at: row.get("created_at")?,
+            })
+        })
+        .optional()
+        .transpose()
+        .ok_or(rusqlite::Error::QueryReturnedNoRows)
+        .flatten()
+        .wrap()
+}
+
+fn get_package(db: &mut Connection, package: PackageId) -> Result<Option<StorePackage>, Error> {
+    db.prepare_cached(Queries::GET_PACKAGE)
+        .wrap()?
+        .query_one(named_params! { ":id": package.as_bytes() }, |row| {
+            Ok(StorePackage {
+                id: PackageId::from_bytes(row.get("id")?),
+                artifact: ArtifactId::from_bytes(row.get("artifact")?),
+                created_at: row.get("created_at")?,
+            })
+        })
+        .optional()
+        .wrap()
+}
+
+// TODO: reimplement this in a way that cant break in 200 different ways
+fn register_artifact(
+    db: &mut Connection,
+    root: PathBuf,
+    archive: Vec<Event>,
+) -> Result<StoreArtifact, Error> {
+    let temp = artifact_path(root.clone(), &random_hash());
+    let file = File::create_new(&temp).compat().wrap()?;
+
+    let mut file = BufWriter::new(file);
+    let mut buffer = bytes::BytesMut::with_capacity(1024 * 4);
+    let mut encoder = xh_archive::encoding::Encoder::new();
+
+    for event in &archive {
+        buffer.clear();
+        encoder.encode(&mut buffer, event);
+        file.write_all(&buffer).compat().wrap()?;
+    }
+
+    let digest = encoder.digest();
+    std::fs::rename(temp, artifact_path(root, &digest))
+        .compat()
+        .wrap()?;
+
+    db.execute(
+        Queries::REGISTER_ARTIFACT,
+        named_params! {
+            ":id": digest.as_bytes(),
+            ":created_at": Timestamp::now()
+        },
+    )
+    .wrap()?;
+
+    Ok(StoreArtifact {
+        id: digest,
+        created_at: Timestamp::now(),
+    })
+}
+
+fn get_artifact(db: &mut Connection, artifact: ArtifactId) -> Result<Option<StoreArtifact>, Error> {
+    db.query_one(
+        Queries::GET_ARTIFACT,
+        named_params! { ":id": artifact.as_bytes() },
+        |row| {
+            Ok(StoreArtifact {
+                id: ArtifactId::from_bytes(row.get("id")?),
+                created_at: row.get("created_at")?,
+            })
+        },
+    )
+    .optional()
+    .wrap()
+}
+
+fn decode_artifact(root: PathBuf, artifact: ArtifactId) -> Result<Option<Vec<Event>>, Error> {
+    let file = match File::open(artifact_path(root, &artifact)) {
+        Ok(file) => file,
+        Err(err) if err.kind() == std::io::ErrorKind::NotFound => return Ok(None),
+        Err(err) => return Err(err).compat().wrap(),
+    };
+
+    let mut mmap = Bytes::from_owner(unsafe { memmap2::Mmap::map(&file).compat().wrap()? });
+    let archive = Decoder::new()
+        .decode_iter(&mut mmap)
+        .collect::<Result<Vec<_>, _>>()
+        .wrap()?;
+
+    Ok(Some(archive))
+}
+
+fn processing_thread(mut db: Connection, mut rx: mpsc::Receiver<Task>) {
+    while let Some(task) = rx.blocking_recv() {
+        debug!("processing task: {task:?}");
+
+        match task {
+            Task::RegisterPackage {
+                package,
+                artifact,
+                channel,
+            } => {
+                let _ = channel.send(register_package(&mut db, package, artifact));
+            }
+            Task::GetPackage { package, channel } => {
+                let _ = channel.send(get_package(&mut db, package));
+            }
+            Task::RegisterArtifact {
+                archive,
+                root,
+                channel,
+            } => {
+                let _ = channel.send(register_artifact(&mut db, root, archive));
+            }
+            Task::GetArtifact { artifact, channel } => {
+                let _ = channel.send(get_artifact(&mut db, artifact));
+            }
+            Task::DecodeArtifact {
+                artifact,
+                root,
+                channel,
+            } => {
+                let _ = channel.send(decode_artifact(root, artifact));
+            }
+            Task::Shutdown => break,
+        }
+    }
+}
+
+/// A store using SQLite as a database, and locally stored artifacts
+pub struct SqliteStore {
+    tx: mpsc::Sender<Task>,
+    root: PathBuf,
+}
+
+impl SqliteStore {
+    pub fn new(root: PathBuf) -> Result<Self, Error> {
+        let root = root.join("artifacts");
+        ensure_dir(&root).compat().wrap()?;
+
+        let db = Connection::open(root.join("store.db")).wrap()?;
+        db.execute_batch(include_str!("initialize.sql")).wrap()?;
+
+        let (tx, rx) = mpsc::channel(16);
+
+        std::thread::Builder::new()
+            .name("sqlite-store-processor".to_string())
+            .spawn(move || processing_thread(db, rx))
+            .wrap()?;
+
+        Ok(Self { root, tx })
+    }
+
+    async fn queue<R>(
+        &self,
+        task: impl FnOnce(oneshot::Sender<Result<R, Error>>) -> Task,
+    ) -> Result<R, Error> {
+        let (req_tx, resp_rx) = oneshot::channel();
+
+        self.tx.send(task(req_tx)).await.wrap()?;
+        resp_rx.await.wrap().flatten()
+    }
+}
+
+impl Store for SqliteStore {
+    fn register_package(
+        &mut self,
+        package: &PackageId,
+        artifact: &ArtifactId,
+    ) -> impl Future<Output = Result<StorePackage, Error>> {
+        self.queue(|channel| Task::RegisterPackage {
+            package: *package,
+            artifact: *artifact,
+            channel,
+        })
+    }
+
+    fn package(
+        &self,
+        package: &PackageId,
+    ) -> impl Future<Output = Result<Option<StorePackage>, Error>> {
+        self.queue(|channel| Task::GetPackage {
+            package: *package,
+            channel,
+        })
+    }
+
+    fn register_artifact(
+        &mut self,
+        archive: Vec<Event>,
+    ) -> impl Future<Output = Result<StoreArtifact, Error>> {
+        self.queue(|channel| Task::RegisterArtifact {
+            archive,
+            channel,
+            root: self.root.clone(),
+        })
+    }
+
+    fn artifact(
+        &self,
+        artifact: &ArtifactId,
+    ) -> impl Future<Output = Result<Option<StoreArtifact>, Error>> {
+        self.queue(|channel| Task::GetArtifact {
+            artifact: *artifact,
+            channel,
+        })
+    }
+
+    fn download(
+        &self,
+        artifact: &ArtifactId,
+    ) -> impl Future<Output = Result<Option<Vec<Event>>, Error>> {
+        self.queue(|channel| Task::DecodeArtifact {
+            artifact: *artifact,
+            root: self.root.clone(),
+            channel,
+        })
+    }
+}
+
+impl Drop for SqliteStore {
+    fn drop(&mut self) {
+        std::mem::drop(self.tx.send(Task::Shutdown));
+    }
+}
+
+fn artifact_path(mut root: PathBuf, artifact: &ArtifactId) -> PathBuf {
+    root.push(artifact.to_string());
+    root
+}
