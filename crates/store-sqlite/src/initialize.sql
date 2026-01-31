88df0e1f0f6d12d2ab9076dd39705643f00e0475
CelestialCrafter <celestialcraftermc@gmail.com>
2026-01-30 20:22:22 -0600

    (store-sqlite) update artifact reference to id

--- crates/store-sqlite/src/initialize.sql
+++ crates/store-sqlite/src/initialize.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS packages(
     id BLOB PRIMARY KEY NOT NULL,
     artifact BLOB NOT NULL,
     created_at TEXT,
-    FOREIGN KEY(artifact) REFERENCES artifacts(hash)
+    FOREIGN KEY(artifact) REFERENCES artifacts(id)
 );
 COMMIT;
