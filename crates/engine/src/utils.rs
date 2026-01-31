49ac7da13b0ec6146a40ab799fc45280c1d1bb23
CelestialCrafter <celestialcraftermc@gmail.com>
2026-01-30 18:29:41 -0600

    (engine/scheduler) make build ids random hashes

--- crates/engine/src/utils.rs
+++ crates/engine/src/utils.rs
@@ -1,13 +1,6 @@
 pub mod passthru;
 
 #[inline]
-pub fn random_hash() -> blake3::Hash {
-    let mut buffer = [0; blake3::OUT_LEN];
-    fastrand::fill(&mut buffer);
-    blake3::Hash::from_bytes(buffer)
-}
-
-#[inline]
 pub fn ensure_dir(path: impl AsRef<std::path::Path>) -> Result<(), std::io::Error> {
     let path = path.as_ref();
     match std::fs::create_dir(path) {
